e9d588e80f97c3bed6da773355adf698bc8103c4
mohammadSelimReza <srreza1999@gmail.com>
2024-08-04 09:07:37 +0600

    forms and views added

---
+++ bank_transaction/forms.py
@@ -0,0 +1,60 @@
+from typing import Any
+from django import forms
+from .models import TransactionModel
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = TransactionModel
+        fields = ['ammount','transaction_type']
+        
+    def __init__(self,*args, **kwargs):
+        self.account = kwargs.pop('account')
+        super().__init__(*args, **kwargs)
+        self.fields['transaction_type'].disabled = True
+        self.fields['transaction_type'].widget = forms.HiddenInput
+        
+    def save(self, commit=True):
+        self.instance.account = self.account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save()
+    
+    
+class DepositForm(TransactionForm):
+    def clean_ammount(self):
+        min_deposit_ammount = 100
+        ammount = self.cleaned_data["ammount"]
+        if ammount < min_deposit_ammount:
+            raise forms.ValidationError(
+                f'You need to deposit at least {min_deposit_ammount}$'
+            )
+        
+        return ammount
+    
+class WithdrawFrom(TransactionForm):
+    def clean_ammount(self):
+        account = self.account
+        min_withdraw_ammount = 500
+        max_withdraw_ammount = 20000
+        balance = account.balance
+        ammount = self.cleaned_data["ammount"]
+        if ammount < min_withdraw_ammount:
+            raise forms.ValidationError(
+                f"You have to withdraw at least {min_withdraw_ammount}$"
+            )
+        if ammount > max_withdraw_ammount:
+            raise forms.ValidationError(
+                f"You can not withdraw more that {max_withdraw_ammount}$ at a time."
+            )
+        if ammount > balance:
+            raise forms.ValidationError(
+                f"You have {balance}$ in your account."
+            )
+        return ammount
+
+class LoadRequestForm(TransactionForm):
+    def clean_ammount(self):
+        ammount = self.cleaned_data["ammount"]
+        
+        return ammount
+    
+    
--- bank_transaction/views.py
+++ bank_transaction/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from .models import TransactionModel
+from .forms import DepositForm,WithdrawFrom,LoadRequestForm
+from .constrant import LOAN,DEPOSIT,LOAN_PAID,WITHDRAWAL
 # Create your views here.
+class TransactionCreateMixin(LoginRequiredMixin,CreateView):
+    template_name = ''
+    model = TransactionModel
+    title = ''
+    success_url = ''
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'account' : self.request.user.account,
+        })
+        return kwargs
+    
+    def get_conext_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': self.title
+        })
+        
+class DepositMoneyView(TransactionCreateMixin):
+    form_class = DepositForm
+    title = "Deposit"
+    
+    def get_initial(self):
+        
+        initial = {'transaction_type':DEPOSIT}
+        return initial
+    
+    def form_valid(self, form):
+        ammount = form.cleaned_data.get('ammount')
+        account = self.request.user.account
+        account.balance += ammount
+        account.save(
+            update_fields = ['balance']
+        )
+        
+        return super().form_valid(form)
+
+class WithdrawMoneyView(TransactionCreateMixin):
+    form_class = WithdrawFrom
+    title = "Withdraw"
+    
+    def get_initial(self):
+        
+        initial = {'transaction_type':WITHDRAWAL}
+        return initial
+    
+    def form_valid(self, form):
+        ammount = form.cleaned_data.get('ammount')
+        account = self.request.user.account
+        account.balance -= ammount
+        account.save(
+            update_fields = ['balance']
+        )
+        
+        return super().form_valid(form)
+       
